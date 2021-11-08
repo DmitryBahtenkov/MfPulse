68dc1df49540a9b4082b984f1319f0f0399dbb13
DmitryBahtenkov <cool.iridiy12soul@gmail.com>
2021-11-08 20:44:40 +0300

    add db abstractions

---
+++ MfPulse.Assessment/Contract/Ratings/Models/RatingDocument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MfPulse.Mongo.Document;
+
+namespace MfPulse.Assessment.Contract.Ratings.Models
+{
+    public record RatingDocument : IDocumentWithCompanyId
+    {
+        public string Id { get; set; }
+        public bool IsArchived { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string CompanyId { get; set; }
+        public string UserId { get; set; }
+        public int TotalScore { get; set; }
+        public List<AssessmentEmbeddedDocument> Assessments { get; set; } = new();
+    }
+}
---
+++ MfPulse.Assessment/Contract/Ratings/Services/IRatingService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace MfPulse.Assessment.Contract.Ratings.Services
+{
+    public interface IRatingService
+    {
+        public Task CreateDefault(string companyId);
+        public Task Delete(string id);
+    }
+}
