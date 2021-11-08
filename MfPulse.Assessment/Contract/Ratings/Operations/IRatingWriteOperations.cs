901bb50697642f488eb2bba74c0f23fa0ca9c497
DmitryBahtenkov <cool.iridiy12soul@gmail.com>
2021-11-08 20:44:40 +0300

    add db abstractions

---
+++ MfPulse.Assessment/Contract/Ratings/Operations/IRatingWriteOperations.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using MfPulse.Assessment.Contract.Ratings.Models;
+using MfPulse.Mongo.Operations.Abstractions;
+
+namespace MfPulse.Assessment.Contract.Ratings.Operations
+{
+    public interface IRatingWriteOperations : IWriteOperations<RatingDocument>
+    {
+        public Task<RatingDocument> AddAssessment(string id, AssessmentEmbeddedDocument document);
+        public Task<RatingDocument> AddAssessments(string id, params AssessmentEmbeddedDocument[] documents);
+    }
+}
