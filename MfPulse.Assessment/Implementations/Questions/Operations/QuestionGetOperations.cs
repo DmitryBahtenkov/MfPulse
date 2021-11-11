2eddb12d4247ed95980f263c81e0092e60b36ad2
DmitryBahtenkov <cool.iridiy12soul@gmail.com>
2021-11-11 08:01:28 +0300

    add impl

---
+++ MfPulse.Assessment/Implementations/Questions/Operations/QuestionGetOperations.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MfPulse.Assessment.Contract.Questions.Models;
+using MfPulse.Assessment.Contract.Questions.Operations;
+using MfPulse.Auth.Contract.Groups.Models;
+using MfPulse.Mongo.Operations;
+using MfPulse.Mongo.Operations.Implementations;
+using MfPulse.Mongo.Security;
+
+namespace MfPulse.Assessment.Implementations.Questions.Operations
+{
+    public class QuestionGetOperations : GetOperations<QuestionDocument>, IQuestionGetOperations
+    {
+        public QuestionGetOperations(DbContext dbContext, MongoSecurityFilter mongoSecurityFilter) : base(dbContext, mongoSecurityFilter)
+        {
+        }
+
+        public async Task<List<QuestionDocument>> ByQuestionaire(string questionaireId)
+        {
+            return await Many(F.Eq(x => x.QuestionaireId, questionaireId));
+        }
+    }
+}
